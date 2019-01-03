a12a841b5cbb3ba906f453aa32397033bf67b4bb
brendanxwhitaker <whitaker.213@osu.edu>
2019-01-03 14:30:56 -0500

    files

---
+++ 353-garnet.sh
@@ -0,0 +1,7 @@
+
+#srun -J rando --mem 4000 -c 4 -w feldspar python3 sim353.py ./embeddings/top_10000_emb.txt Word2Vec
+#srun -J rando --mem 50000 -c 10 -w adamantium python3 sim999.py ./embeddings/gigatext1.txt Word2Vec
+#srun -J sim353ld --mem 4000 -c 4 -w garnet python3 sim353.py ../embeddings/GoogleNews-vectors-negative300.bin Word2Vec
+#srun -J sim353ld --mem 4000 -c 4 -w garnet python3 sim353.py ../embeddings/wiki-news-300d-1M-subword.txt Word2Vec
+srun -J rando --mem 4000 -c 4 -w garnet python3 sim353.py ../embeddings/random__source--wiki-news-300d-1M-subword__2019-01-03-1134.txt Word2Vec 
+#srun -J rando --mem 50000 -c 10 -w adamantium python3 sim353.py ../embeddings/glove.840B.300d.word2vec.txt Word2Vec 
---
+++ sim999.py
@@ -0,0 +1,127 @@
+#I had to add the pyemblib thing to my path each time I login
+#the instructions to do this are in the github repo for pyemblib
+#I checked 3 different words and it seems each vector has exactly
+#100 components so I'm just going to assume we're in $\mathbb{R}^100$.
+#Beginning to implement similarity/relatedness. 
+import re
+import pyemblib
+import math
+import scipy.stats as stats
+import sys
+
+emb_path = sys.argv[1]
+
+
+def testFunc():
+	print('testFun')
+
+wordsim = open("SimLex-999.txt")
+
+print("Preprocessing. ")
+file_name_length = len(emb_path)
+last_char = emb_path[file_name_length - 1]
+
+# Decide if it's a binary or text embedding file, and read in
+# the embedding as a dict object, where the keys are the tokens
+# (strings), and the values are the components of the corresponding 
+# vectors (floats).
+embedding = {}
+if (last_char == 'n'):
+    embedding = pyemblib.read(emb_path, mode=pyemblib.Mode.Binary, replace_errors=True)
+elif (last_char == 't'):
+    embedding = pyemblib.read(emb_path, mode=pyemblib.Mode.Text, replace_errors=True)
+else:
+    print("Unsupported embedding format. ")
+    exit()
+
+
+
+
+
+
+
+result = []
+humanRank = []
+cosineRank = []
+numMiss = 0
+
+'''
+for line in wordsim.read().splitlines():
+	lineArray = re.split(r'\t+',line)
+	print(lineArray)
+'''
+
+#'''
+for line in wordsim.read().splitlines():
+	#skips the headers
+	if line[0] != '#':
+		#the first column is the word type
+		#the second column is the first word, followed by the second word
+		#the third line is the human-rated similarity for comparison
+		lineArray = re.split(r'\t+',line)
+		#print(lineArray)
+		wordA = lineArray[0]
+		wordB = lineArray[1]
+		humanRk = lineArray[3]
+		
+		#lengthA = len(lineArray[1])
+		#lengthB = len(lineArray[2])
+		dotProd = 0
+		if wordA in embedding and wordB in embedding:
+			vecA = embedding[wordA]
+			vecB = embedding[wordB]
+			
+			dotSum = 0
+			sumSquaresA = 0
+			sumSquaresB = 0
+			for i in range(0,100):
+				dotSum += vecA[i] *  vecB[i]
+				sumSquaresA += (vecA[i])**2
+				sumSquaresB += (vecB[i])**2
+			
+			magA = math.sqrt(sumSquaresA)
+			magB = math.sqrt(sumSquaresB)
+			dotProd = dotSum/(magA*magB)
+
+			newRow = []
+			for x in range(0,4):
+				newRow.append(lineArray[x])
+			newRow.append(dotProd)
+			result.append(newRow)
+			cosineRank.append(dotProd)
+			humanRank.append(humanRk)
+			
+		else:
+			dotProd = 0
+			numMiss += 1
+		#print dotProd		
+#for y in range(0,10):
+	#print(result[y])
+
+print('the length of cosineRank is: ' + str(len(cosineRank)))
+print('the length of humanRank is: ' + str(len(humanRank)))
+rho = stats.spearmanr(humanRank,cosineRank)
+print(rho)
+
+print(numMiss)
+#'''
+
+#Here's some sample code for opening the word vectors from the binary file:
+'''
+testVec = embedding['population']
+print testVec
+lengthOfTestVec = len(testVec)
+print lengthOfTestVec
+vec2 = embedding['shower']
+vec3 = embedding['architecture']
+len2 = len(vec2)
+len3 = len(vec3)
+print len2
+print len3
+print vec2[0]
+print vec2[1]
+print vec2[2]
+print vec2[99]
+'''
+
+#print 'CD' in embedding
