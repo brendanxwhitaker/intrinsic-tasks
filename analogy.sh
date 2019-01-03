06e74337a016e183595082cc400a06a350174137
brendanxwhitaker <whitaker.213@osu.edu>
2019-01-03 18:55:16 -0500

    pathcorrected

--- analogy.sh
+++ analogy.sh
@@ -1,2 +1,2 @@
 # Analogy task. 
-srun -w adamantium -c 4 --mem 4000 -J analogy ./compute-accuracy glove.840B.300d.word2vec_clean.bin < questions-words.txt
+srun -w adamantium -c 4 --mem 4000 -J analogy ./compute-accuracy ../pretrained/glove.840B.300d.word2vec_clean.bin < questions-words.txt
